cfec2c346b1b2e1432e2ac463686547ce9de1e7e
Julio Santos <julio@morgane.com>
2019-11-09 15:12:29 -0800

    Refactor and fetch lyrics

--- nick-cave-lyrics.py
+++ nick-cave-lyrics.py
@@ -12,22 +12,35 @@ class NickCaveLyricsParser():
 
     def fetch_albums(self):
         albums = self.soup.find_all('div', {'class': 'lyric-release-wrap-big'})
-        self.album_names = []
-        for album in albums:
-            album_data = album.find_all('h3')
-            album_title = album_data[0].contents[0]
-            print('>>>', album_title)
-            self.album_names.append(album_title)
-            album_songs = album.find_all('a')
-            for song_data in album_songs:
-                song_title = song_data.contents[0]
-                print(song_title)
-
+        self.record_albums(albums)
         return albums
 
-    def get_album_names(self):
-        return self.album_names
+    def record_albums(self, albums):
+        for album in albums:
+            self.record_album(album)
 
-parser = NickCaveLyricsParser()
-album_names = parser.get_album_names()
-print(album_names)
+    def record_album(self, album):
+        album_data = album.find_all('h3')
+        album_songs = album.find_all('a')
+        self.record_song_lyrics(album_data, album_songs)
+
+    def record_song_lyrics(self, album_data, album_songs):
+        album_title = album_data[0].contents[0]
+        for song_data in album_songs:
+            song_title = song_data.contents[0]
+            link_to_song_lyrics = song_data['href']
+            response_from_song_lyrics_page = requests.get(link_to_song_lyrics)
+            song_lyrics_page = BeautifulSoup(response_from_song_lyrics_page.text, 'html.parser')
+            title = song_lyrics_page.find_all('h1')[0].contents[0]
+            year = song_lyrics_page.find_all('div', {'class': 'date'})[0].contents[0]
+            lyrics = song_lyrics_page.find_all('div', {'class': 'lyrics'})[0].find_all('p')
+            self.__record_lyrics(album_title, title, year, lyrics)
+
+    def __record_lyrics(self, album_title, song_title, year, lyrics):
+        print(album_title, '>', song_title, '>', year)
+        for paragraph in lyrics:
+            print(paragraph.contents)
+
+if __name__ == '__main__':
+    parser = NickCaveLyricsParser()
+    parser.record_albums()
